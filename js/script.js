a55521dafc4f11b219acedc8ea9db8ee84f26ce7
utsavyadav6677 <utsavyadav6677@gmail.com>
2024-12-03 15:50:52 +0530

    Initial commit of portfolio

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Toggle Navigation Menu
+document.addEventListener("DOMContentLoaded", () => {
+    const navToggle = document.querySelector(".nav-toggle");
+    const navMenu = document.querySelector(".nav-menu");
+  
+    navToggle.addEventListener("click", () => {
+      navMenu.classList.toggle("active");
+    });
+  });
+  
